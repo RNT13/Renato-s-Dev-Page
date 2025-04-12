78837b7a35621f5090f1d0859350ec307deb694c
RNT13 <renatornt13@gmail.com>
2025-04-12 17:09:05 -0300

    Fix: case 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,20 @@
-import React from 'react'
-import { Provider } from 'react-redux'
-import About from './components/About/About'
-import Education from './components/Education/Education'
-import Footer from './components/Footer/Footer'
-import Header from './components/Header/Header'
-import Hero from './components/Hero/Hero'
-import Languages from './components/Languages/Languages'
-import Projects from './components/Projects/Projects'
-import Skills from './components/Skills/Skills'
-import Social from './components/Social/Social'
-import Store from './redux/store'
-import { GlobalContainer, GlobalStyle } from './styles/globalStyle'
+import { Provider } from "react-redux"
+import About from "./components/About/About"
+import Education from "./components/Education/Education"
+import Footer from "./components/Footer/Footer"
+import Header from "./components/Header/Header"
+import Hero from "./components/Hero/Hero"
+import Languages from "./components/Languages/Languages"
+import Projects from "./components/Projects/Projects"
+import Skills from "./components/Skills/Skills"
+import Social from "./components/Social/Social"
+import store from "./redux/store"
+import { GlobalContainer, GlobalStyle } from "./styles/globalStyle"
+
 
 const App: React.FC = () => {
   return (
-    <Provider store={Store}>
+    <Provider store={store}>
       <GlobalStyle />
       <Header />
       <Hero />
